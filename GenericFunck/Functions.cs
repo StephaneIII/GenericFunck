effc0f7df6f2b9480c719d4363e7187db138ac7b
Stéphane Karlsen <stk002@edu.zealand.dk>
2024-02-07 09:47:13 +0100

    d

---
+++ GenericFunck/Functions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericFunck
+{
+    public class Functions
+    {
+
+
+        public static int FindLargest(List<int> listOfInt)
+        {
+            int LargestNumber = listOfInt[0];
+            foreach (int i in listOfInt)
+            {
+                if (LargestNumber < i)
+                    LargestNumber = i;
+            }
+
+            return LargestNumber;
+
+        }
+
+    }
+}
--- GenericFunck/Program.cs
+++ GenericFunck/Program.cs
@@ -1,2 +1,7 @@
-﻿//List<int> intList = new List<int>() { 5, 10, 3, 8, 15 };
-//Console.WriteLine("Largest element in intList: " + Functions.FindLargest(intList)); // Output: 15
+﻿using GenericFunck;
+
+
+List<int> intList = new List<int>() { 5, 10, 3, 8, 15 };
+Console.WriteLine("Largest element in intList: " + Functions.FindLargest(intList)); // Output: 15
+
+
